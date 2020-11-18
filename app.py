78d97fd0acda22c3cebd944e8c7af075eaea6a53
Cory Ortega <coryortega@Corys-MacBook-Pro.local>
2020-11-18 14:24:07 -0800

    added psycopg2 connection

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import os
+import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from GMD_PS5 import scrape
@@ -10,8 +11,10 @@ load_dotenv(find_dotenv())
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bsdvckxoaifygn:0f9228e3f1c12b105402ccfde819b2d73e853007ab93c00223f3c8a563070d21@ec2-54-82-208-124.compute-1.amazonaws.com:5432/de6pk3rpal71ti'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
 
 text_alert = "GO GET DAT PLAYSTATION 5 \n\nhttps://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG?ref_=ast_sto_dp"
